6265fcb1147d5a7c4d2daa1b1df1c538d54098fb
Glenn Hurd <glennqhurd@gmail.com>
2015-09-22 10:32:38 -0400

    Cleaned up symmetry_test and tictactoe_symmetry further.

--- symmetry_test.py
+++ symmetry_test.py
@@ -5,62 +5,24 @@ import unittest
 from tictactoe_symmetry import *
 
 
-class testRoboplayer(unittest.TestCase):
+class testSymmetry(unittest.TestCase):
     def testApplySymmetries(self):
-        boardString = ('XO       ')
-        identityString = applySymmetry(boardString, 0)
-        self.assertEqual(boardString, identityString)
-        rot90String = applySymmetry(boardString, 1)
-        self.assertEqual(('  X  O   '), rot90String)
-        rot180String = applySymmetry(boardString, 2)
-        self.assertEqual(('       OX'), rot180String)
-        rot270String = applySymmetry(boardString, 3)
-        self.assertEqual(('   O  X  '), rot270String)
-        verticalString = applySymmetry(boardString, 4)
-        self.assertEqual((' OX      '), verticalString)
-        horizontalString = applySymmetry(boardString, 5)
-        self.assertEqual(('      XO '), horizontalString)
-        leftDiagonalString = applySymmetry(boardString, 6)
-        self.assertEqual(('     O  X'), leftDiagonalString)
-        rightDiagonalString = applySymmetry(boardString, 7)
-        self.assertEqual(('X  O     '), rightDiagonalString)
+        boardString = 'XO       '
+        self.assertEqual(boardString, applySymmetry(boardString, IDENTITY))
+        self.assertEqual('  X  O   ', applySymmetry(boardString, ROT90))
+        self.assertEqual('       OX', applySymmetry(boardString, ROT180))
+        self.assertEqual('   O  X  ', applySymmetry(boardString, ROT270))
+        self.assertEqual(' OX      ', applySymmetry(boardString, VERTICAL))
+        self.assertEqual('      XO ', applySymmetry(boardString, HORIZONTAL))
+        self.assertEqual('     O  X', applySymmetry(boardString, LEFT_DIAGONAL))
+        self.assertEqual('X  O     ', applySymmetry(boardString, RIGHT_DIAGONAL))
 
-    def testRotation(self):
-        boardString = ('XO       ')
-        identityString = identity(boardString)
-        self.assertEqual(boardString, identityString)
-        Rot90String = Rot90(boardString)
-        self.assertEqual(('  X  O   '), Rot90String)
-        Rot180String = Rot180(boardString)
-        self.assertEqual(('       OX'), Rot180String)
-        Rot90String = Rot90(Rot90String)
-        self.assertEqual(('       OX'), Rot90String)
-        Rot270String = Rot270(boardString)
-        self.assertEqual(('   O  X  '), Rot270String)
-        Rot90String = Rot90(Rot90String)
-        self.assertEqual(('   O  X  '), Rot90String)
+    def testSortBoards(self):
+        self.assertEqual(['       OX', '      XO ', '     O  X', '   O  X  ', '  X  O   ',
+                          ' OX      ', 'X  O     ', 'XO       '], sortBoards('XO       '))
 
-    def testVerticalReflection(self):
-        boardString = '         '
-        newString = flipVertical(boardString)
-        self.assertEqual(boardString, newString)
-        boardString = 'X O X    '
-        newString = flipVertical(boardString)
-        self.assertEqual('O X X    ', newString)
-        boardString = 'X O O X O'
-        newString = flipVertical(boardString)
-        self.assertEqual('O X O O X', newString)
-
-    def testHorizontalReflection(self):
-        boardString = '         '
-        newString = flipHorizontal(boardString)
-        self.assertEqual(boardString, newString)
-        boardString = 'X O X    '
-        newString = flipHorizontal(boardString)
-        self.assertEqual('    X X O', newString)
-        boardString = 'X O O O X'
-        newString = flipHorizontal(boardString)
-        self.assertEqual('O X O X O', newString)
+    def testCanonicalBoard(self):
+        self.assertEqual('        X', canonicalBoard('X        '))
 
 
 if __name__ == '__main__':
--- tictactoe_symmetry.py
+++ tictactoe_symmetry.py
@@ -1,6 +1,6 @@
 __author__ = 'Glenn'
 
-IDENTITY_TUPLE = (0, 1, 2, 3, 4, 5, 6, 7, 8)
+IDENTITY_TUPLE = range(9)
 ROT90_TUPLE = (6, 3, 0, 7, 4, 1, 8, 5, 2)
 ROT180_TUPLE = (8, 7, 6, 5, 4, 3, 2, 1, 0)
 ROT270_TUPLE = (2, 5, 8, 1, 4, 7, 0, 3, 6)
@@ -10,78 +10,20 @@ LEFT_DIAGONAL_TUPLE = (8, 5, 2, 7, 4, 1, 6, 3, 0)
 RIGHT_DIAGONAL_TUPLE = (0, 3, 6, 1, 4, 7, 2, 5, 8)
 SYMMETRY_TUPLE = (IDENTITY_TUPLE, ROT90_TUPLE, ROT180_TUPLE, ROT270_TUPLE, VERTICAL_TUPLE, HORIZONTAL_TUPLE,
                   LEFT_DIAGONAL_TUPLE, RIGHT_DIAGONAL_TUPLE)
+IDENTITY, ROT90, ROT180, ROT270, VERTICAL, HORIZONTAL, LEFT_DIAGONAL, RIGHT_DIAGONAL = range(8)
 
 # Applies symmetry 0-7 to the boardString supplied
-# 0 identity
-# 1 Rot90
-# 2 Rot180
-# 3 Rot270
-# 4 flipVertical
-# 5 flipHorizontal
-# 6 flipDiagonalFromLeft
-# 7 flipDiagonalFromRight
 def applySymmetry(boardString, symmetry):
-    tuple = SYMMETRY_TUPLE[symmetry]
-    boardList1 = list(boardString)
-    boardList2 = [boardList1[tuple[i]] for i in range(len(boardString))]
-    return ''.join(boardList2)
+    return ''.join([boardString[SYMMETRY_TUPLE[symmetry][i]] for i in range(len(boardString))])
 
 
-def identity(boardString):
-    return boardString
+def sortBoards(boardString):
+    return sorted((applySymmetry(boardString, i) for i in range(len(SYMMETRY_TUPLE))))
 
 
-def Rot90(boardString):
-    boardList1 = list(boardString)
-    boardList2 = []
-    for i in range(len(boardString)):
-        boardList2.append(boardList1[ROT90_TUPLE[i]])
-    return ''.join(boardList2)
+def canonicalBoard(boardString):
+    return sortBoards(boardString)[0]
 
 
-def Rot180(boardString):
-    boardList1 = list(boardString)
-    boardList2 = []
-    for i in range(len(boardString)):
-        boardList2.append(boardList1[ROT180_TUPLE[i]])
-    return ''.join(boardList2)
-
-
-def Rot270(boardString):
-    boardList1 = list(boardString)
-    boardList2 = []
-    for i in range(len(boardString)):
-        boardList2.append(boardList1[ROT270_TUPLE[i]])
-    return ''.join(boardList2)
-
-
-def flipVertical(boardString):
-    boardList1 = list(boardString)
-    boardList2 = []
-    for i in range(len(boardString)):
-        boardList2.append(boardList1[VERTICAL_TUPLE[i]])
-    return ''.join(boardList2)
-
-
-def flipHorizontal(boardString):
-    boardList1 = list(boardString)
-    boardList2 = []
-    for i in range(len(boardString)):
-        boardList2.append(boardList1[HORIZONTAL_TUPLE[i]])
-    return ''.join(boardList2)
-
-
-def flipDiagonalFromLeft(boardString):
-    boardList1 = list(boardString)
-    boardList2 = []
-    for i in range(len(boardString)):
-        boardList2.append(boardList1[LEFT_DIAGONAL_TUPLE[i]])
-    return ''.join(boardList2)
-
-
-def flipDiagonalFromRight(boardString):
-    boardList1 = list(boardString)
-    boardList2 = []
-    for i in range(len(boardString)):
-        boardList2.append(boardList1[RIGHT_DIAGONAL_TUPLE[i]])
-    return ''.join(boardList2)
+if __name__ == '__main__':
+    print applySymmetry('X        ', RIGHT_DIAGONAL)
